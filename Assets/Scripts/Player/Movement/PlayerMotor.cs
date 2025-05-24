c3246a10693c15897ce66ef3e9b82532f581e7bd
Daniel Benson <dbenson24@gmail.com>
2025-05-23 21:12:26 -0500

    made bad changes to player motor

--- Assets/Scripts/Player/Movement/PlayerMotor.cs
+++ Assets/Scripts/Player/Movement/PlayerMotor.cs
@@ -3,168 +3,167 @@ using UnityEngine;
 [RequireComponent(typeof(CharacterController))]
 public class PlayerMotor : MonoBehaviour
 {
-	[SerializeField] private PlayerMotorSettings _settings;
-	[SerializeField] private Transform _camera;
-	[SerializeField] private bool _debug;
-	
-	private CharacterController _motor;
+    [Header("Core Settings")]
+    [SerializeField] private PlayerMotorSettings _settings;
+    [SerializeField] private Transform _camera;
+    [SerializeField] private bool _debug;
 
-	private float _cameraRotX;
-	private bool _jumpKeyPressed;
-	private bool _playerGrounded;
-	
-	private Vector3 _velocity;
-	private Vector3 _lastVelocity;
-	
-	private RaycastHit _downHit;
-	private (Vector3 dir, float angle) _slope;
-	private float _verticalVelocity;
-	
-	private void Awake()
-	{
-		_motor = GetComponent<CharacterController>();
+    [Header("Reset Settings")]
+    [Tooltip("Y-position threshold below which the player resets")]
+    [SerializeField] private float fallResetHeight = -10f;
+    [Tooltip("Y-position the player is moved to upon reset")]
+    [SerializeField] private float resetPositionHeight = 10f;
 
-		if (_settings.lockCursorOnAwake)
-		{
-			Cursor.lockState = CursorLockMode.Locked;
-			Cursor.visible = false;
-		}
-	}
+    [Header("Gravity & Jump Settings")]
+    [Tooltip("Downward force to stick to the ground when grounded")]
+    [SerializeField] private float stickyGroundForce = 0.5f;
 
-	private void Update()
-	{
-		if (transform.position.y < -10)
-		{
-			_motor.enabled = false;
-			transform.position = Vector3.up * 10f;
-			_motor.enabled = true;
-		}
-		
-		_slope = SlopeDirection();
-		Physics.SphereCast(transform.position, _settings.groundRaycastRadius, Vector3.down, out _downHit, _settings.groundRaycastDistance, ~LayerMask.GetMask("Player"));
-		_playerGrounded = _motor.isGrounded;
-		
-		// only want to jump if we are grounded
-		if (_settings.canJump && _playerGrounded && Input.GetKeyDown(_settings.jumpKey))
-		{
-			_jumpKeyPressed = true;
-		}
-		
-		HandleGravityAndJump();
-		HandlePosition();
-		HandleCameraAndBodyRotation();
-	}
-	
-	private void OnApplicationFocus(bool focus)
-	{
-		if (_settings.lockCursorOnFocus)
-		{
-			Cursor.lockState = CursorLockMode.Locked;
-			Cursor.visible = false;
-		}
-	}
-	
-	private void OnGUI()
-	{
-		if (_debug)
-		{
-			GUILayout.Label("IsGrounded: " + _motor.isGrounded);
-			GUILayout.Label("Slope Angle: " + _slope.angle);
-			GUILayout.Label("Velocity: " + transform.InverseTransformDirection(_velocity));
-		}
-	}
+    [Header("Ski Mode Settings")]
+    [Tooltip("Mouse button index to hold for skiing (0=left, 1=right, etc.)")]
+    [SerializeField] private int skiMouseButton = 1;
+    [Tooltip("Speed multiplier applied when skiing")]
+    [SerializeField] private float skiSpeedMultiplier = 1.5f;
+    [Tooltip("Friction coefficient when skiing (typically zero)")]
+    [SerializeField] private float skiFrictionCoefficient = 0f;
 
-	private void OnDrawGizmos()
-	{
-		if (_debug)
-		{
-			var original = Gizmos.color;
-			Gizmos.color = Color.green;
+    [Header("Air & Slope Settings")]
+    [Tooltip("Multiplier for movement when airborne")]
+    [SerializeField] private float airMovementMultiplier = 0.1f;
+    [Tooltip("Factor used to scale slope influence")]
+    [SerializeField] private float slopeBoostFactor = 0.00278f;
 
-			Gizmos.DrawRay(transform.position, _velocity);
-			
-			Gizmos.color = original;
-		}
-	}
+    private CharacterController _motor;
+    private float _cameraRotX;
+    private bool _jumpKeyPressed;
+    private bool _playerGrounded;
+    private Vector3 _velocity;
+    private Vector3 _lastVelocity;
+    private RaycastHit _downHit;
+    private (Vector3 dir, float angle) _slope;
+    private float _verticalVelocity;
 
-	private void HandleGravityAndJump()
-	{
-		if (_playerGrounded)
-		{
-			// add a sticky force to make our player stick to the ground
-			// should improve behaviour on downwards slopes
-			// this is basically the equal and opposite force when an 
-			// object is grounded
-			_verticalVelocity = -0.5f;
-			
-			if (_jumpKeyPressed)
-			{
-				// apply a jump force to the player
-				_verticalVelocity = _settings.jumpForce;
-				_jumpKeyPressed = false;
-			}
-		}
-		else
-		{
-			// apply gravity to the player
-			_verticalVelocity -= _settings.gravity * Time.deltaTime;
-		}
-	}
+    private void Awake()
+    {
+        _motor = GetComponent<CharacterController>();
+        if (_settings.lockCursorOnAwake)
+        {
+            Cursor.lockState = CursorLockMode.Locked;
+            Cursor.visible = false;
+        }
+    }
 
-	private void HandlePosition()
-	{
-		// this is just our input vector
-		var moveAcceleration = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
-		moveAcceleration = Vector3.ClampMagnitude(moveAcceleration, 1);
-		moveAcceleration = transform.TransformDirection(AdjustedMovementAcceleration(moveAcceleration));
+    private void Update()
+    {
+        HandleFallReset();
+        SampleSlope();
+        HandleInputs();
+        HandleGravityAndJump();
+        HandlePosition();
+        HandleCameraAndBodyRotation();
+    }
 
-		// normal acceleration * time
-		var normalVelocityDelta = _playerGrounded ? moveAcceleration * Time.deltaTime : moveAcceleration * (0.1f * Time.deltaTime);
-		
-		var slopeVelocityDelta = _slope.angle > _settings.minAngleForSlope ? 
-			_slope.dir * (_slope.angle * 0.00278f * _settings.slopeAcceleration * Time.deltaTime) : 
-			Vector3.zero;
+    private void HandleFallReset()
+    {
+        if (transform.position.y < fallResetHeight)
+        {
+            _motor.enabled = false;
+            transform.position = Vector3.up * resetPositionHeight;
+            _motor.enabled = true;
+        }
+    }
 
-		var frictionalCoefficient = _playerGrounded ? _settings.frictionOnGround : _settings.frictionInAir;
-		
-		// current acceleration * coefficient * time
-		var frictionalVelocityDelta = -_lastVelocity / Time.deltaTime * (frictionalCoefficient * Time.deltaTime);
-		
-		_velocity.y = _verticalVelocity;
+    private void SampleSlope()
+    {
+        _slope = SlopeDirection();
+        Physics.SphereCast(
+            transform.position,
+            _settings.groundRaycastRadius,
+            Vector3.down,
+            out _downHit,
+            _settings.groundRaycastDistance,
+            ~LayerMask.GetMask("Player")
+        );
+        _playerGrounded = _motor.isGrounded;
+    }
 
-		_velocity += normalVelocityDelta + frictionalVelocityDelta + slopeVelocityDelta;
+    private void HandleInputs()
+    {
+        if (_settings.canJump && _playerGrounded && Input.GetKeyDown(_settings.jumpKey))
+            _jumpKeyPressed = true;
+    }
 
-		_motor.Move(_velocity);
+    private void HandleGravityAndJump()
+    {
+        if (_playerGrounded)
+        {
+            // Sticky force for ground contact
+            _verticalVelocity = -stickyGroundForce;
+            if (_jumpKeyPressed)
+            {
+                _verticalVelocity = _settings.jumpForce;
+                _jumpKeyPressed = false;
+            }
+        }
+        else
+        {
+            // Apply gravity when in air
+            _verticalVelocity -= _settings.gravity * Time.deltaTime;
+        }
+    }
 
-		_lastVelocity = _velocity;
-	}
+    private void HandlePosition()
+    {
+        bool skiing = Input.GetMouseButton(skiMouseButton) && _playerGrounded;
+        float speedMod = skiing ? skiSpeedMultiplier : 1f;
 
-	private Vector3 AdjustedMovementAcceleration(Vector3 movementAcceleration)
-	{
-		movementAcceleration *= _settings.movementAcceleration;
-		movementAcceleration.x *= _settings.horizontalAccelerationWeight;
-		movementAcceleration.z *= _settings.verticalAccelerationWeight;
-		return movementAcceleration;
-	}
+        // Input direction
+        var input = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+        input = Vector3.ClampMagnitude(input, 1);
+        input = transform.TransformDirection(AdjustedMovementAcceleration(input));
 
-	private void HandleCameraAndBodyRotation()
-	{
-		var mouse = new Vector2(Input.GetAxis("Mouse X"), Input.GetAxis("Mouse Y"));
-		
-		// clamp the extents of the cameras up and down look axis
-		_cameraRotX = Mathf.Clamp(_cameraRotX - mouse.y, _settings.minCameraTilt, _settings.maxCameraTilt);
-		
-		// rotate the player around the y axis
-		transform.Rotate(0, mouse.x, 0);
+        // Movement delta (ground vs air)
+        var movementDelta = _playerGrounded
+            ? input * speedMod * Time.deltaTime
+            : input * speedMod * airMovementMultiplier * Time.deltaTime;
 
-		// rotate the camera around the x axis
-		_camera.localRotation = Quaternion.Euler(_cameraRotX, _camera.localEulerAngles.y, _camera.localEulerAngles.z);
-	}
+        // Slope influence
+        var slopeDelta = (_slope.angle > _settings.minAngleForSlope)
+            ? _slope.dir * (_slope.angle * slopeBoostFactor * _settings.slopeAcceleration * Time.deltaTime)
+            : Vector3.zero;
 
-	private (Vector3 direction, float angle) SlopeDirection()
-	{
-		var downSlope = Vector3.Cross(_downHit.normal, Vector3.up);
-		downSlope = Vector3.Cross(_downHit.normal, downSlope);
-		return (downSlope, Vector3.Angle(downSlope, Vector3.up));
-	}
+        // Friction
+        float frictionCoef = !_playerGrounded
+            ? _settings.frictionInAir
+            : (skiing ? skiFrictionCoefficient : _settings.frictionOnGround);
+        var frictionDelta = -_lastVelocity / Time.deltaTime * (frictionCoef * Time.deltaTime);
+
+        // Combine and move
+        _velocity.y = _verticalVelocity;
+        _velocity += movementDelta + frictionDelta + slopeDelta;
+        _motor.Move(_velocity);
+        _lastVelocity = _velocity;
+    }
+
+    private Vector3 AdjustedMovementAcceleration(Vector3 acc)
+    {
+        acc *= _settings.movementAcceleration;
+        acc.x *= _settings.horizontalAccelerationWeight;
+        acc.z *= _settings.verticalAccelerationWeight;
+        return acc;
+    }
+
+    private void HandleCameraAndBodyRotation()
+    {
+        var mouse = new Vector2(Input.GetAxis("Mouse X"), Input.GetAxis("Mouse Y"));
+        _cameraRotX = Mathf.Clamp(_cameraRotX - mouse.y, _settings.minCameraTilt, _settings.maxCameraTilt);
+        transform.Rotate(0, mouse.x, 0);
+        _camera.localRotation = Quaternion.Euler(_cameraRotX, _camera.localEulerAngles.y, _camera.localEulerAngles.z);
+    }
+
+    private (Vector3 direction, float angle) SlopeDirection()
+    {
+        var downSlope = Vector3.Cross(_downHit.normal, Vector3.up);
+        downSlope = Vector3.Cross(_downHit.normal, downSlope);
+        return (downSlope, Vector3.Angle(downSlope, Vector3.up));
+    }
 }
